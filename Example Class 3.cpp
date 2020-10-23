70ed0efefb7396a3e5697fbae2b6339c40cab9f2
Vasil-tech <72305944+Vasil-tech@users.noreply.github.com>
2020-10-23 17:27:37 +0300

    Add files via upload

---
+++ Example Class 3.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream> 
+using namespace std;
+
+class Subtraction 
+{ 
+	public: int funct_Sub(int a, int b); 
+}; 
+class Sum:public Subtraction 
+{
+	public: int funct_Sum(int a, int b); 
+}; 
+int Subtraction::funct_Sub(int a, int b) 
+{ 
+	return a - b; 
+} 
+int Sum::funct_Sum(int a, int b) 
+{
+	return a + b; 
+} 
+int main() 
+{ 
+	setlocale(LC_ALL, "Russian");
+	Sum s;
+	int a = 5; 
+	int b = 3; 
+	cout << "Разность = " << s.funct_Sub(a, b) << endl;
+	cout << "Сумма = " << s.funct_Sum(a, b) << endl; 
+	return 0;
+}
