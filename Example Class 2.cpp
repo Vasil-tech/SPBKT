09fa34e27ea64f91f66309e1b58d3c9d9abe1889
Vasil-tech <72305944+Vasil-tech@users.noreply.github.com>
2020-10-23 15:54:58 +0300

    Add files via upload

---
+++ Example Class 2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <chrono>
+
+Class date
+{
+	private:int,day,year
+};
+public: int, input(int, char, int); 
+	  int output(int, char*, int); 
+	  int sum1(int, char*, int); 
+	  int sum2(int, char*, int); 
+	  int min1(int, char*, int); 
+	  int min2(int, char*, int); 
+	  int koi(int, char*, int, int, char*, int, int)
