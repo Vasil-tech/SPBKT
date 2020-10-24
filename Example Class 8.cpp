be96304ba1884c7246142525b42c54715a54c3cd
Vasil-tech <72305944+Vasil-tech@users.noreply.github.com>
2020-10-24 23:17:05 +0300

    Add files via upload

---
+++ Example Class 8.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream> 
+
+using namespace std;
+
+class Test 
+{ 
+	public: Test (int = 0); 
+	void print() const; 
+	private: int x; 
+}; 
+Test::Test(int a) 
+{
+	x = a;
+}//конструктор 
+void Test::print() const 
+{ 
+	cout <<"x = "<< x << endl <<"this->x = " << this -> x<<endl <<"(*this).x= "<<(*this).x<<endl; 
+} 
+int main() 
+{ 
+	Test a(12); 
+	a.print(); 
+	return 0; 
+}
