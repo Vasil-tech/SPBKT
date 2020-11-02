b055c8e377fcc6ce4e34356d63acc5a75bbb286e
Vasil-tech <72305944+Vasil-tech@users.noreply.github.com>
2020-11-02 23:17:24 +0300

    Update Lab 15-16.h

--- Lab 15-16.h
+++ Lab 15-16.h
@@ -10,7 +10,7 @@ namespace Project3 {
 	using namespace System::Drawing;
 
 	/// <summary>
-	/// Ñâîäêà äëÿ MyForm
+	/// Сводка для MyForm
 	/// </summary>
 	public ref class MyForm : public System::Windows::Forms::Form
 	{
@@ -19,13 +19,13 @@ namespace Project3 {
 		{
 			InitializeComponent();
 			//
-			//TODO: äîáàâüòå êîä êîíñòðóêòîðà
+			//TODO: добавьте код конструктора
 			//
 		}
 
 	protected:
 		/// <summary>
-		/// Îñâîáîäèòü âñå èñïîëüçóåìûå ðåñóðñû.
+		/// Освободить все используемые ресурсы.
 		/// </summary>
 		~MyForm()
 		{
@@ -45,19 +45,20 @@ namespace Project3 {
 	private: System::Windows::Forms::Label^ label4;
 	private: System::Windows::Forms::TextBox^ textBox4;
 	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::Button^ button3;
 
 	protected:
 
 	private:
 		/// <summary>
-		/// Îáÿçàòåëüíàÿ ïåðåìåííàÿ êîíñòðóêòîðà.
+		/// Обязательная переменная конструктора.
 		/// </summary>
 		System::ComponentModel::Container ^components;
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
-		/// Òðåáóåìûé ìåòîä äëÿ ïîääåðæêè êîíñòðóêòîðà — íå èçìåíÿéòå 
-		/// ñîäåðæèìîå ýòîãî ìåòîäà ñ ïîìîùüþ ðåäàêòîðà êîäà.
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
 		/// </summary>
 		void InitializeComponent(void)
 		{
@@ -72,25 +73,26 @@ namespace Project3 {
 			this->label4 = (gcnew System::Windows::Forms::Label());
 			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
 			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->button3 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(104, 174);
+			this->button1->Location = System::Drawing::Point(102, 166);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(75, 23);
 			this->button1->TabIndex = 0;
-			this->button1->Text = L"Ðàññ÷èòàòü";
+			this->button1->Text = L"Рассчитать";
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(197, 226);
+			this->button2->Location = System::Drawing::Point(197, 233);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(75, 23);
 			this->button2->TabIndex = 1;
-			this->button2->Text = L"Âûõîä";
+			this->button2->Text = L"Выход";
 			this->button2->UseVisualStyleBackColor = true;
 			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
 			// 
@@ -122,7 +124,7 @@ namespace Project3 {
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(125, 13);
 			this->label1->TabIndex = 5;
-			this->label1->Text = L"Ïðîéäåíûå êèëîìåòðû";
+			this->label1->Text = L"Пройденые километры";
 			// 
 			// label2
 			// 
@@ -131,7 +133,7 @@ namespace Project3 {
 			this->label2->Name = L"label2";
 			this->label2->Size = System::Drawing::Size(140, 13);
 			this->label2->TabIndex = 6;
-			this->label2->Text = L"Ðàñõîä òîïëèâà íà 100 êì";
+			this->label2->Text = L"Расход топлива на 100 км";
 			// 
 			// label3
 			// 
@@ -140,12 +142,12 @@ namespace Project3 {
 			this->label3->Name = L"label3";
 			this->label3->Size = System::Drawing::Size(119, 13);
 			this->label3->TabIndex = 7;
-			this->label3->Text = L"Öåíà çà ëèòð áåíçèíà";
+			this->label3->Text = L"Цена за литр бензина";
 			// 
 			// label4
 			// 
 			this->label4->AutoSize = true;
-			this->label4->Location = System::Drawing::Point(16, 226);
+			this->label4->Location = System::Drawing::Point(12, 233);
 			this->label4->Name = L"label4";
 			this->label4->Size = System::Drawing::Size(37, 13);
 			this->label4->TabIndex = 8;
@@ -165,13 +167,24 @@ namespace Project3 {
 			this->label5->Name = L"label5";
 			this->label5->Size = System::Drawing::Size(111, 13);
 			this->label5->TabIndex = 10;
-			this->label5->Text = L"Êîëè÷åñòâî ïîåçäîê";
+			this->label5->Text = L"Количество поездок";
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(197, 166);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(75, 50);
+			this->button3->TabIndex = 11;
+			this->button3->Text = L"Заранее \r\nвведённые \r\nчисла";
+			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &MyForm::button3_Click);
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(284, 261);
+			this->Controls->Add(this->button3);
 			this->Controls->Add(this->label5);
 			this->Controls->Add(this->textBox4);
 			this->Controls->Add(this->label4);
@@ -222,12 +235,31 @@ namespace Project3 {
 			itog = (S * (r / 100) * c) * v;
 		else 
 			itog = S * (r / 100) * c;
-		label4->Text = "Result: " + System::Convert::ToString(itog);
+		label4->Text = "Result: " + System::Convert::ToString(itog) + " Руб";
 
+	}
+	/*
+	*ЗДЕСЬ
+	*Я
+	*ДЕЛАЮ
+	*ПРОГРАММУ
+	*С
+	*ЗАРАНЕЕ
+	*ВВЕДЁННЫМИ
+	*ЧИСЛАМИ
+	*/
+	private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
+		int itog;
+		double S = 240;
+		double r = 8.4;
+		double c = 16.5;
+		itog = S * (r / 100) * c;
+		label4->Text = "Стоимость: " + System::Convert::ToString(itog) + " Руб";
 	}
 	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
 		Application::Exit();
 	}
-	};
+	
+};
 
 }
